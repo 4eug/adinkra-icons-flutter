79322f0151e1b13bf8895918b7dbd2a465bfdb97
Eugene Debrah <debraheug@gmail.com>
2022-04-26 10:16:39 +0000

    ..

--- lib/src/icons.dart
+++ lib/src/icons.dart
@@ -8,5 +8,132 @@ class AdinkraIcons {
   //Aban icon
   static const aban = AdinkraIcons._('aban');
 
-  //
+  //Abe-Dua icon
+  static const abeDua = AdinkraIcons._('abe-dua');
+
+  //Adinkrahene icon
+  static const adinkrahene = AdinkraIcons._('adinkrahene');
+
+  //Adwo icon
+  static const adwo = AdinkraIcons._('adwo');
+
+  //Agyindawuru icon
+  static const agyindawuru = AdinkraIcons._('agyindawuru');
+
+  //Akoben icon
+  static const akoben = AdinkraIcons._('akoben');
+
+  //Akofena icon
+  static const akofena = AdinkraIcons._('akofena');
+
+  //Akoko-Nan icon
+  static const akokoNan = AdinkraIcons._('akoko-nan');
+
+  //Akoma-Ntoso icon
+  static const akomaNtoso = AdinkraIcons._('akoma-ntoso');
+
+  //Akoma icon
+  static const akoma = AdinkraIcons._('akoma');
+
+  //Ananse-Ntontan icon
+  static const ananseNtontan = AdinkraIcons._('ananse-ntontan');
+
+  //Ani-Bere-A-Enso-Gya icon
+  static const aniBereAEnsoGya = AdinkraIcons._('ani-bere-a-enso-gya');
+
+  //Asase-Ye-duru icon
+  static const asaseYeDuru = AdinkraIcons._('asase-ye-duru');
+
+  //Aya icon
+  static const aya = AdinkraIcons._('aya');
+
+  //Bese-saka icon
+  static const beseSaka = AdinkraIcons._('bese-saka');
+
+  //Bi-Nka-Bi icon
+  static const biNkaBi = AdinkraIcons._('bi-nka-bi');
+
+  //Boa-Me-Na-Me-Mmoa-Wo icon
+  static const boaMeNaMeMmoaWo = AdinkraIcons._('boa-me-na-me-mmoa-wo');
+
+  //Dame-Dame icon
+  static const dameDame = AdinkraIcons._('dame-dame');
+
+  //Denkyem icon
+  static const denkyem = AdinkraIcons._('denkyem');
+
+  //Dono-Ntoaso icon
+  static const donoNtoaso = AdinkraIcons._('dono-ntoaso');
+
+  //Dono icon
+  static const dono = AdinkraIcons._('dono');
+
+  //Duafe icon
+  static const duafe = AdinkraIcons._('duafe');
+
+  //Dwennimmen icon
+  static const dwennimmen = AdinkraIcons._('dwennimmen');
+
+  //Eban icon
+  static const eban = AdinkraIcons._('eban');
+
+  //Epa icon
+  static const epa = AdinkraIcons._('epa');
+
+  //Ese-Ne-Tekrema icon
+  static const eseNeTekrema = AdinkraIcons._('ese-ne-tekrema');
+
+  //Fafanto icon
+  static const fafanto = AdinkraIcons._('fafanto');
+
+  //Fawohodie icon
+  static const fawohodie = AdinkraIcons._('fawohodie');
+
+  //Fihankra icon
+  static const fihankra = AdinkraIcons._('fihankra');
+
+  //Fofo icon
+  static const fofo = AdinkraIcons._('fofo');
+
+  //Funtunfunefu-Denkyemfunefu icon
+  static const funtunfunefuDenkyemfunefu = AdinkraIcons._('funtunfunefu-denkyemfunefu');
+
+  //Gye-Nyame icon
+  static const gyeNyame = AdinkraIcons._('gye-nyame');
+
+  //Hwe-Mu-Dua icon
+  static const hweMuDua = AdinkraIcons._('hwe-mu-dua');
+
+  //Hye-Won-Hye icon
+  static const hyeWonHye = AdinkraIcons._('hye-won-hye');
+
+  //Kete-Pa icon
+  static const ketePa = AdinkraIcons._('kete-pa');
+
+  //Kintinkantan icon
+  static const kintinkantan = AdinkraIcons._('kintinkantan');
+
+  //Kramo-Bone-Amma-Yeanhu-Kramo-Pa icon
+  static const kramoBoneAmmaYeanhuKramoPa = AdinkraIcons._('kramo-bone-amma-yeanhu-kramo-pa');
+
+  //Kuronti-Ne-Akwamu icon
+  static const kurontiNeAkwamu = AdinkraIcons._('kuronti-ne-akwamu');
+
+  //Kwatakye-Atiko icon
+  static const kwatakyeAtiko = AdinkraIcons._('kwatakye-atiko');
+
+  //Mako icon
+  static const mako = AdinkraIcons._('mako');
+
+  //Mate-Masie icon
+  static const mateMasie = AdinkraIcons._('mate-masie');
+
+  //Me-Ware-Wo icon
+  static const meWareWo = AdinkraIcons._('me-ware-wo');
+
+  //Menso-Wo-Kenten icon 
+  static const mensoWoKenten = AdinkraIcons._('menso-wo-kenten');
+
+  //Mframadan icon
+  static const mframadan = AdinkraIcons._('mframadan');
 }
