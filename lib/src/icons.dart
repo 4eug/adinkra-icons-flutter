1df0e615501245978bf32198a1d189057e6a77e3
Eugene Debrah <debraheug@gmail.com>
2022-04-26 09:47:35 +0000

    ..

---
+++ lib/src/icons.dart
@@ -0,0 +1,12 @@
+library adinkra_icons;
+
+class AdinkraIcons {
+  const AdinkraIcons._(this.name);
+
+  final String name;
+
+  //Aban icon
+  static const aban = AdinkraIcons._('aban');
+
+  //
+}
