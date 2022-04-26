63b773b511668b5cad89a184c5ab82745f30b43e
Eugene Debrah <debraheug@gmail.com>
2022-04-26 09:26:26 +0000

    icons set

--- lib/adinkra_icons.dart
+++ lib/adinkra_icons.dart
@@ -1,7 +1,37 @@
 library adinkra_icons;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:path/path.dart' as p;
+
+import 'src/icons.dart';
+
+export 'src/icons.dart';
+class AdinkraIcon extends StatelessWidget {
+  const AdinkraIcon(
+    this.icon, {
+    this.color,
+    this.size,
+    this.solid = false,
+  });
+
+  final AdinkraIcons icon;
+  final Color? color;
+  final double? size;
+  final bool solid;
+
+  @override
+  Widget build(BuildContext context) {
+    final path = p.join(
+      'packages/Adinkraicons/assets/${solid ? 'solid' : 'outline'}/',
+      icon.name,
+    );
+    return SvgPicture.asset(
+      '$path.svg',
+      color: color ?? IconTheme.of(context).color,
+      width: size,
+      height: size,
+      alignment: Alignment.center,
+    );
+  }
 }
